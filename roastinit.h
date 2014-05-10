4b362b39ab931841a86f9047a2fc3a2bcb55d581
Andrew Schomin <Schomin@Andrews-MacBook-Air.local>
2014-05-09 20:56:25 -0600

    add roast init files

---
+++ roastinit.h
@@ -0,0 +1,15 @@
+//****************************************************************************
+//*                    MSP430 Popcorn Popper Coffee Roaster                  *
+//****************************************************************************
+//
+//           module:  roastinit.h
+//      description:  header file toInitialize the roast process
+//  original author:  Andrew Schomin
+//    creation date:  05-09-2014
+//
+//****************************************************************************
+
+// === INCLUDES ==============================================================
+
+#include <msp430.h>
+#include <legacymsp430.h>
